1026c3fe4ca8481ab30bb358d00d1cad31ee8390
Pieter <pieter.schutte@structureit.net>
2013-08-16 16:07:13 +0200

    first stab a NewRelic publisher

---
+++ Puppy.Monitoring.NewRelicPublisher/NewRelicCounterPublisher.cs
@@ -0,0 +1,37 @@
+﻿using Puppy.Monitoring.Events;
+using Puppy.Monitoring.Publishing;
+
+namespace Puppy.Monitoring.NewRelicPublisher
+{
+    internal static class NewRelicMetricName
+    {
+        public static string Create(IEvent @event)
+        {
+            return string.Format("{0}/{1}", @event.Categorisation.Category, @event.Categorisation.SubCategory);
+        }
+    }
+
+    internal class NewRelicCounterPublisher : IPublishEvents
+    {
+        public void Publish(IEvent @event)
+        {
+            NewRelic.Api.Agent.NewRelic.IncrementCounter(NewRelicMetricName.Create(@event));
+        }
+    }
+
+    internal class NewRelicResponseTimePublisher : IPublishEvents
+    {
+        public void Publish(IEvent @event)
+        {
+            NewRelic.Api.Agent.NewRelic.RecordResponseTimeMetric(NewRelicMetricName.Create(@event), @event.Timings.Took);
+        }
+    }
+
+    internal class NewRelicErrorPublisher : IPublishEvents
+    {
+        public void Publish(IEvent @event)
+        {
+            NewRelic.Api.Agent.NewRelic.NoticeError(NewRelicMetricName.Create(@event));
+        }
+    }
+}
---
+++ Puppy.Monitoring.NewRelicPublisher/NewRelicPublisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Logging;
+using Puppy.Monitoring.Events;
+using Puppy.Monitoring.Publishing;
+
+namespace Puppy.Monitoring.NewRelicPublisher
+{
+    public class NewRelicPublisher : IPublishEvents
+    {
+        private static readonly ILog log = LogManager.GetLogger<NewRelicPublisher>();
+        private readonly IDictionary<Func<IEvent, bool>, IPublishEvents> factory = new Dictionary<Func<IEvent, bool>, IPublishEvents>()
+            {
+                {@event => @event.Timings.Took == int.MinValue, new NewRelicCounterPublisher()},
+                {@event => @event.GetType() == typeof(FailureEvent), new NewRelicErrorPublisher()},
+                {@event => @event.Timings.Took != int.MinValue, new NewRelicResponseTimePublisher()},
+            };
+
+        public void Publish(IEvent @event)
+        {
+            var publisher = factory.FirstOrDefault(f => f.Key(@event));
+            if (publisher.Value == null)
+            {
+                log.DebugFormat("Failed to get a New Relic publisher for {0}", @event);
+                return;
+            }
+
+            publisher.Value.Publish(@event);
+        }
+    }
+}
