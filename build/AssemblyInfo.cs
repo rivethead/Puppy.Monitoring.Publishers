4a7301794168d0b281601ae0644719e0464b0055
Pieter <pieter.schutte@structureit.net>
2013-08-16 15:35:59 +0200

    update build to deploy to nuget.org

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -5,8 +5,9 @@ using System.Security;
 [assembly: AssemblyDescription("Puppy.Monitoring")]
 [assembly: AssemblyProduct("Puppy.Monitoring")]
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.71")]
+[assembly: AssemblyFileVersion("0.1.73")]
 
+[assembly: AssemblyInformationalVersion("0.1.73.f3b704")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
-[assembly: AssemblyInformationalVersion("0.1.71.000000")]
+
